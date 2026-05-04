9f8e170ce356c453873bec44172efc1aff1e1dfe
Manish <manishpandey6239@gmail.com>
2026-05-04 23:09:35 +0530

    added overlay for better text visibility

--- apps/web/src/pages/HomePage.jsx
+++ apps/web/src/pages/HomePage.jsx
@@ -181,7 +181,7 @@ const HomePage = () => {
           className="relative min-h-[100dvh] flex items-center pt-[80px]"
           style={{
           backgroundImage: isMobile
-          ? 'linear-gradient(rgba(255,255,255,0.20), rgba(255,255,255,0.20)), url("https://i.pinimg.com/736x/51/f0/9a/51f09aa54ab809c11c73279f02889b79.jpg")'
+          ? 'linear-gradient(rgba(rgba(0,0,0,0.5), rgba(rgba(0,0,0,0.5)), url("https://i.pinimg.com/736x/51/f0/9a/51f09aa54ab809c11c73279f02889b79.jpg")'
           : 'linear-gradient(rgba(255,255,255,0.20), rgba(255,255,255,0.20)), url("https://horizons-cdn.hostinger.com/f2268395-5bcb-4cfc-98ed-965a4845c225/780897d1452054cf07dbbf10bd981152.jpg")',
           backgroundSize: 'cover',
           backgroundPosition: isMobile ? 'top' : 'center',
